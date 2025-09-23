70af681715c92e37de0403338b83271967e621b0
Shubham Kumar Sah <shubham.sah2212@gmail.com>
2025-09-24 00:00:41 +0530

    update

--- rough.py
+++ rough.py
@@ -1,21 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
+
 root = tk.Tk()
 
 root.geometry("300x300")
 
-sel = tk.StringVar()
-sel.set("Select")
+f = tk.Frame(root)
+f.pack(fill="both", expand=True)
 
-def com():
-    value = sel.get()
-    print(value)
+c = tk.Canvas(f)
+c.pack(side="left", fill="both", expand=True)
 
-tk.Button(root, text="Print", command=com).pack(padx=50, pady=50)
+sb = tk.Scrollbar(c, orient="vertical", command=c.yview)
+sb.pack(side="right", fill="y")
+
+sf = tk.Scrollbar(c)
+sf.pack()
+
+# sel = tk.StringVar()
+# sel.set("Select")
+
+# def com():
+#     value = sel.get()
+#     print(value)
+
+# tk.Button(root, text="Print", command=com).pack(padx=50, pady=50)
 
 
 
-cb = ttk.Combobox(root, textvariable=sel, values=("A", "B", "C"))
-cb.pack(padx=20, pady=20)
+# cb = ttk.Combobox(root, textvariable=sel, values=("A", "B", "C"))
+# cb.pack(padx=20, pady=20)
 
 root.mainloop()
+
