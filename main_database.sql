a08af77aad1dc3c85adefc46cc38f080538f5ab8
Shubham Kumar Sah <shubham.sah2212@gmail.com>
2025-10-12 00:22:27 +0530

    update

--- main_database.sql
+++ main_database.sql
@@ -2,42 +2,45 @@ use train;
 
 show tables;
 
-create table train(
-
-train_no varchar(5) not null primary key,
-train_name varchar(50) not null,
-fare varchar(3) not null
-
-);
-
-create table train_stations(
-
-id integer primary key auto_increment,
-train_no varchar(5) not null,
-station varchar(50) not null,
-arrival time not null,
-departure time not null,
-sequence integer not null
-
-);
-
-desc train;
-
-desc train_stations;
-
-insert into train values('13319', 'Dhanbad–Ranchi Express', '120');
-
-insert into train_stations(train_no, station, arrival, departure, sequence) 
-values('13319', 'RNC - Ranchi', '12:55', '12:55', 20);
+-- TABLE - 1 : login -> User Data
+desc login;
 
 select * from login;
 
-select * from train;
+-- TABLE - 2 : trains -> List of Trains
+create table trains(
 
-select * from train_stations;
+id int auto_increment primary key,
+train_no varchar(5) not null,
+train_name varchar(30) not null,
+source_station varchar(30) not null,
+destination_station varchar(30) not null,
+total_distance int not null,
+avg_speed float not null
 
-update train_stations set distance = '157' where id = 12;
+);
 
-alter table train add avg_speed float;
+desc trains;
 
-update train set source_station = 'Ranchi' where train_no = '13319';
+select * from trains;
+
+insert into trains(train_no, train_name, source_station, destination_station, total_distance, avg_speed)
+values('18620', 'INTERCITY EXP', 'GODDA - GODA', 'RANCHI - RNC', 457, 56);
+
+-- TABLE - 3 : route -> Route of a train
+create table route(
+train_no varchar(5) not null,
+station_code varchar(10) not null,
+sequence varchar(2) not null,
+distance int not null
+);
+
+desc route;
+
+-- TABLE - 4 : class_fare -> Fare of different class types like 1AC, 2AC, 3AC, SL, etc.
+create table class_fare(
+class_code varchar(5) not null,
+rate_per_km int not null,
+reservation_charge int not null,
+gst_percent float not null
+);
--- rough.py
+++ rough.py
@@ -1,27 +1,34 @@
-import mysql.connector as con
-import tkinter as tk
+# import mysql.connector as con
+# import tkinter as tk
 
-# con1 = con.connect(host = "localhost", user = "root", password = "shubham@1234", database = "train")
-# cur1 = con1.cursor()
+# # con1 = con.connect(host = "localhost", user = "root", password = "shubham@1234", database = "train")
+# # cur1 = con1.cursor()
 
-# sql = "select * from train"
-# cur1.execute(sql)
+# # sql = "select * from train"
+# # cur1.execute(sql)
 
-# rec = cur1.fetchall()
+# # rec = cur1.fetchall()
 
-# print(rec[1])
+# # print(rec[1])
 
-def prt():
-    print("Hello World!")
+# def prt():
+#     print("Hello World!")
 
-root = tk.Tk()
-root.geometry("500x500")
+# root = tk.Tk()
+# root.geometry("500x500")
 
-can = tk.Canvas(root, width=300, height=300, bg="yellow")
-can.pack(pady=50)
+# can = tk.Canvas(root, width=300, height=300, bg="yellow")
+# can.pack(pady=50)
 
-tk.Label(can, text="Hello World").place(x=50, y=50)
+# tk.Label(can, text="Hello World").place(x=50, y=50)
 
-tk.Button(can, text="Click!", command=prt).place(x=50, y=100)
+# tk.Button(can, text="Click!", command=prt).place(x=50, y=100)
 
-root.mainloop()
+# root.mainloop()
+
+name = "Deoghar - DGHR"
+
+if "DGHR" in name:
+    print('Y')
+else:
+    print('N')
